b703b2f28c26bb02a2971dafe47f2bb774bed304
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-19 14:50:36 -0400

    add nextui and brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,21 @@
 import type { Config } from 'tailwindcss'
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      
+      colors: {
+        darkBrown: "#362a20",
+        lightBrown: "#f2cfb1",
+        mainOrange: "#ffa500"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,6 +23,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 export default config
