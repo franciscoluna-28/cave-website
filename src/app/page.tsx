2c4ca01c533521cbaea8ad9e6a24eed82ea4fade
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-19 14:50:08 -0400

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    // Added leading to the text. I consider this is important in devices like computers
+    <main className="flex min-h-screen flex-col items-center justify-center px-8 ">
+      <h1 className="text-lightBrown font-bold text-4xl text-center leading-relaxed sm:w-1/2">
+        Colaborative Artistic Ventures Entertainment
+      </h1>
+      <p className="text-lightBrown text-center leading-relaxed">
+        A community of artists working together to make art and tell stories.
+      </p>
+      <Button className="px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-2xl bg-mainOrange mt-4">
+        Join CAVE
+      </Button>
+      {/*       <Header /> */}
+      {/*       <Button className=" bg-main">Welcome!</Button> */}
     </main>
-  )
+  );
 }
