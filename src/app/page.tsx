3be15f7c996c6d61f3730aca48754448b382e3a1
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-11-19 15:29:07 -0400

    add test information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { Button, ButtonGroup } from "@nextui-org/react";
 import Header from "@/components/Header";
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
+import LandingPageCard from "@/components/landing-page/Card";
+import Test from "../../public/test.jpeg";
 
 export default function Home() {
   return (
@@ -15,8 +18,20 @@ export default function Home() {
       <Button className="px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-2xl bg-mainOrange mt-4">
         Join CAVE
       </Button>
-      {/*       <Header /> */}
-      {/*       <Button className=" bg-main">Welcome!</Button> */}
+
+      
+      <section className="w-full justify-center flex flex-col items-center mt-8">
+        <h2 className="text-lightBrown font-bold text-4xl text-center leading-relaxed sm:w-1/2">
+          See more
+        </h2>
+        <LandingPageCard
+          title="Projects"
+          small="Learn more about our projects"
+          imageURL={Test}
+          subTitle="Cave Projects"
+          imageAlt="Car"
+        />
+      </section>
     </main>
   );
 }
