d7e59df758196a32aaba754efb3e89d40c6f5ec3
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-12-27 21:41:49 -0400

    add forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 import { Button, ButtonGroup } from "@nextui-org/react";
 import Header from "@/components/Header";
-import { Card, CardHeader, CardBody } from "@nextui-org/react";
-import LandingPageCard from "@/components/landing-page/Card";
-import Test from "../../public/test.jpeg";
+import {Textarea} from "@nextui-org/react";
+import {Input} from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -13,25 +12,38 @@ export default function Home() {
         Colaborative Artistic Ventures Entertainment
       </h1>
       <p className="text-lightBrown text-center leading-relaxed">
-        A community of artists working together to make art and tell stories.
+        A community of artists working together to make art and tell stories!
       </p>
       <Button className="px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-2xl bg-mainOrange mt-4">
-        Join CAVE
+        Join the Server
       </Button>
 
       
       <section className="w-full justify-center flex flex-col items-center mt-8">
-        <h2 className="text-lightBrown font-bold text-4xl text-center leading-relaxed sm:w-1/2">
+{/*         <h2 className="text-lightBrown font-bold text-4xl text-center leading-relaxed sm:w-1/2">
           See more
-        </h2>
-        <LandingPageCard
+        </h2> */}
+{/*         <LandingPageCard
           title="Projects"
           small="Learn more about our projects"
           imageURL={Test}
           subTitle="Cave Projects"
           imageAlt="Car"
-        />
+        /> */}
       </section>
+      <h2 className="text-lightBrown font-bold text-4xl text-center leading-relaxed sm:w-1/2">
+        Contact
+      </h2>
+
+        <form className="space-y-6 mt-4 min-w-[300px]">
+          <div className="flex flex-col space-y-6">
+        <input className="px-2 p-2 rounded-lg bg-lightBrown border border-b-4 border-r-4 border-black placeholder:text-black" type="text" placeholder="Subject"></input>
+        <textarea placeholder="Message" className="px-2 p-2 min-h-[200px] border border-b-4 border-r-4 border-black bg-lightBrown rounded-lg placeholder:text-black"></textarea>
+        </div> 
+        <Button className="px-5 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-2xl bg-mainOrange mt-4">
+        Send
+      </Button>
+        </form>
     </main>
   );
 }
